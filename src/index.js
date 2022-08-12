00d4b4d82dddbd32ac6b03e871cd0601a546e599
ReichertA2 <aydee0112@yahoo.com>
2022-08-11 23:54:58 -0500

    added Navbar and theme components

--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+import primaryDarkTheme from './themes/primaryDarkTheme';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={primaryDarkTheme}>
+      <CssBaseline/>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
