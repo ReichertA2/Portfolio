ee1d64a5f0d60ed6672011a81213e65958362ea4
ReichertA2 <aydee0112@yahoo.com>
2022-08-13 15:20:13 -0500

    added the mainbody component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import MainBody from './components/MainBody';
 import NavBar from './components/NavBar';
+
+
 
 function App() {
   return (
     <div>
+      
       <NavBar></NavBar>
+      <MainBody />
+
       
     </div>
   );
---
+++ src/components/MainBody.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Button from "@mui/material/Button";
+
+export default function MainBody() {
+  return (
+    <>
+    <Button sx={{textAlign:"center"}} variant="contained">Full Stack Developer</Button>
+    <p>I am a Software Developer focused in React-Frontend and Flask-Backend utilizing PostgreSQL. I am creative, innovative and passionate about bringing solutions to challenging issues. I will continue to strive to improve and be better as a developer. Building the right team has always been essential to my continued success. I am interested in either both Front-End and Back-end. Enjoy how front-end brings the beauty of an application and love how backend makes the connection. Cannot wait to join your team!!
+    </p>
+    </>
+
+   
+  );
+}
