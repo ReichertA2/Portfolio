d7b8e0c9f5f48bb723af3c6f9e1f849e8fafdd92
ReichertA2 <aydee0112@yahoo.com>
2022-08-20 01:31:19 -0500

    changed flip cards background color

--- src/App.js
+++ src/App.js
@@ -8,24 +8,27 @@ import Contact from './components/Contact';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles.scss";
 import FlipCard from "./components/FlipCard";
+import ResumeScanner from "../src/ResumeScanner.PNG"
+import Pokemon from "../src/Pokemon.PNG"
+import Pokemon2 from "../src/Pokemon2.PNG"
 
 const cards = [
   {
     id: "1",
     variant: "hover",
-    front: "Hover",
-    back: "Back"
+    front: <img src={ResumeScanner} alt={" resume scanner project"} />,
+    back: <h1>Resume Scanner</h1>
   },
   {
     id: "2",
     variant: "hover",
-    front: "Hover",
+    front: <img src={Pokemon} alt={" pokemon application"} />,
     back: "Back"
   },
   {
     id: "3",
     variant: "hover",
-    front: "Hover",
+    front: <img src={Pokemon2} alt={" pokemon application"} />,
     back: "Back"
   }
 ];
