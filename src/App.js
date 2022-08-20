4313643a527eb5afab2027e7b359885a7bcb69d0
ReichertA2 <aydee0112@yahoo.com>
2022-08-19 22:34:15 -0500

    added flipcard component

--- src/App.js
+++ src/App.js
@@ -5,8 +5,30 @@ import Experience from './components/Experience';
 import AboutMe from './components/AboutMe';
 import SkillsTitle from './components/SkillsTitle';
 import Contact from './components/Contact';
-import Project from './components/Project'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles.scss";
+import FlipCard from "./components/FlipCard";
 
+const cards = [
+  {
+    id: "1",
+    variant: "hover",
+    front: "Hover",
+    back: "Back"
+  },
+  {
+    id: "2",
+    variant: "click",
+    front: "Click",
+    back: "Back"
+  },
+  {
+    id: "3",
+    variant: "focus",
+    front: "Focus",
+    back: "Back"
+  }
+];
 
 
 
@@ -21,7 +43,7 @@ function App() {
       <SkillsTitle/>
       <AboutMe/>
       <Experience />
-      <Project />
+      <FlipCard />
       <Contact />
     
     
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,7 +8,7 @@ export default function Contact() {
     <section style={{background: '#2D2F33', textAlign: 'center', marginTop: 100}}>
         <h1 id="contact"  className='mt-24' style={{ fontSize:25, display: 'inline-block'}}>Looking for new and exciting opportunities.</h1>
         <h1 style={{ fontSize:20, color:'rgba(255, 255, 255, 0.7)'}}>Look forward to hearing from you.</h1>
-        <a href="mailto:aydee0112@yahoo.com"><Button style={{marginTop:15, marginBottom: 100}} variant='outlined'> <EmailIcon sx={{marginRight:"0.5rem"}}/>Contact Me</Button>
+        <a style={{textDecoration:'none'}} href="mailto:aydee0112@yahoo.com"><Button style={{marginTop:15, marginBottom: 100}} variant='outlined'> <EmailIcon sx={{marginRight:"0.5rem"}}/>Contact Me</Button>
         
         </a>
     
