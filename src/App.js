8aacbdd6843983aadf1acaaf173188e92f2c50d9
ReichertA2 <aydee0112@yahoo.com>
2022-08-21 13:07:01 -0500

    moved project image from right to middle

--- src/App.js
+++ src/App.js
@@ -19,48 +19,48 @@ const cards = [
   {
     id: "1",
     variant: "hover",
-    front: <img src={ResumeScannerComputer3} alt={" resume scanner project"} />,
+    front: <img src={PokemonComputer} alt={"reading list"} />,
     back: (
       <div style={{ textAlign: "center" }}>
-        <WorkIcon style={{ color: "#7deaa2" }}></WorkIcon>
-        <h1 style={{ fontSize: 20 }}>Resume Scanner</h1>
-        <p
-          style={{
-            fontSize: 15,
-            color: "rgba(255, 255, 255, 0.7)",
-            fontWeight: 400,
-          }}
-        >
-          Application that scans resume and multiple job descriptions to provide
-          the matching and missing keyword/phrases to help user get through the applicant
-          tracking system.
-        </p>
-        <p style={{ fontSize: 14 }}>
-          Flask, Python, React, JavaScript, PostgreSQL, MUI, CSS, HTML, Formik, Yup
-        </p>
-        {/* <a href=></a> add link of resume scanner and link to code*/}
-      </div>
+      <AutoStoriesIcon style={{ color: "#7deaa2" }}></AutoStoriesIcon>
+      <h1 style={{ fontSize: 20 }}>Reading List</h1>
+      <p
+        style={{
+          fontSize: 15,
+          color: "rgba(255, 255, 255, 0.7)",
+          fontWeight: 400,
+        }}
+      >
+        Application that allows users to browse, remove, and add books to their reading list.
+      </p>
+      <p style={{ fontSize: 14 }}>API, React, JavaScript, MUI, CSS, HTML</p>
+      {/* <a href=></a> add link of resume scanner and link to code*/}
+    </div>
     ),
   },
   {
     id: "2",
     variant: "hover",
-    front: <img src={PokemonComputer} alt={" reading list"} />,
+    front: <img src={ResumeScannerComputer3} alt={" resume scanner"} />,
     back: (
       <div style={{ textAlign: "center" }}>
-        <AutoStoriesIcon style={{ color: "#7deaa2" }}></AutoStoriesIcon>
-        <h1 style={{ fontSize: 20 }}>Reading List</h1>
-        <p
-          style={{
-            fontSize: 15,
-            color: "rgba(255, 255, 255, 0.7)",
-            fontWeight: 400,
-          }}
-        >
-          Application that allows users to browse, remove, and add books to their reading list.
-        </p>
-        <p style={{ fontSize: 14 }}>API, React, JavaScript, MUI, CSS, HTML</p>
-        {/* <a href=></a> add link of resume scanner and link to code*/}
+      <WorkIcon style={{ color: "#7deaa2" }}></WorkIcon>
+      <h1 style={{ fontSize: 20 }}>Resume Scanner</h1>
+      <p
+        style={{
+          fontSize: 15,
+          color: "rgba(255, 255, 255, 0.7)",
+          fontWeight: 400,
+        }}
+      >
+        Application that scans resume and multiple job descriptions to provide
+        the matching and missing keyword/phrases to help user get through the applicant
+        tracking system.
+      </p>
+      <p style={{ fontSize: 14 }}>
+        Flask, Python, React, JavaScript, PostgreSQL, MUI, CSS, HTML, Formik, Yup
+      </p>
+      {/* <a href=></a> add link of resume scanner and link to code*/}
       </div>
     ),
   },
@@ -98,7 +98,7 @@ function App() {
       <SkillsTitle />
       <AboutMe />
       <Experience />
-      <div id='project'className="container">My Projects
+      <div id='project'className="container">
         <div className="row h-100">
           <div class="col d-flex flex-column flex-md-row justify-content-around align-items-center">
             {cards.map((card) => (
@@ -113,3 +113,5 @@ function App() {
 }
 
 export default App;
+
+
